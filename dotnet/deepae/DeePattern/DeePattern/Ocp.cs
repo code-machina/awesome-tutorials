741d1195eaca7e52b5037db1d1bb32c2b8f70e8e
gbkim <gbkim1988@gmail.com>
2018-12-06 06:37:41 +0900

    OCp

---
+++ dotnet/deepae/DeePattern/DeePattern/Ocp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeePattern.SOLID.OCP.VIOLATION
+{
+    /// <summary>
+    /// 제품의 색깔에 대한 열거 타입 정의
+    /// </summary>
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    /// <summary>
+    /// 제품의 크기에 대한 열거 타입 정의
+    /// </summary>
+    public enum Size
+    {
+        Small, Medium, Large, Yuge
+    }
+
+    /// <summary>
+    /// Product 클래스는 제품의 이름 색깔, 크기를 멤버로 한다.
+    /// 개별 제품의 사양(Spec) 정보를 포함하여 사용자가 원하는 정보를 
+    /// 필터하여 보여준다.
+    /// </summary>
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">유 </param>
+        /// <param name="color"></param>
+        /// <param name="size"></param>
+        public Product(string name, Color color, Size size)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+    }
+
+    public class Ocp
+    {
+
+    }
+}
