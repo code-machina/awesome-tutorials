be9f6f2fa5a7ca0a37105dc493500f95f6f073ff
gbkim <gbkim1988@gmail.com>
2018-12-08 20:00:57 +0900

    오늘의 마무리 ...

--- dotnet/deepae/DeePattern/TestDeePa/TestDecorator.cs
+++ dotnet/deepae/DeePattern/TestDeePa/TestDecorator.cs
@@ -74,93 +74,93 @@ namespace TestDeePa
              * */
         }
 
-        [Test]
-        public void Test_BookSelling_With_Decorator()
-        {
-            /* 시나리오 구축
-             * 
-             * 결국 궁극적인 목표는 개발자가 비즈니스 로직을 구현할 때 사용하기 쉬운
-             * 구조를 만드는 것이다. 결국 나의 고객은 개발자이다.
-             * 
-             * 결국은 복잡한 포인트
-             * 
-             * 전제 조건
-             * - 책의 종류에 따라, 할인율이 다르다.
-             * - 회원의 종류에 따라, 할인율이 다르다.
-             * - 책의 가격에 따라, 할인율이 다르다.
-             * - 회원이 사용하는 포인트에 따라 제품의 가격이 다르다.
-             * - 포인트의 종류는 다양하다. 
-             * 
-             * 클래스 종류
-             * - Book 클래스
-             *   - 가격 
-             *   - 상품 코드
-             *   - 책 제목
-             * - OriginalBookPrice
-             *   
-             * - User 클래스, Book 클래스
-             *   - 테스트를 위해 익명 클래스를 선언한다.
-             * */
+[Test]
+public void Test_BookSelling_With_Decorator()
+{
+    /* 시나리오 구축
+        * 
+        * 결국 궁극적인 목표는 개발자가 비즈니스 로직을 구현할 때 사용하기 쉬운
+        * 구조를 만드는 것이다. 결국 나의 고객은 개발자이다.
+        * 
+        * 결국은 복잡한 포인트
+        * 
+        * 전제 조건
+        * - 책의 종류에 따라, 할인율이 다르다.
+        * - 회원의 종류에 따라, 할인율이 다르다.
+        * - 책의 가격에 따라, 할인율이 다르다.
+        * - 회원이 사용하는 포인트에 따라 제품의 가격이 다르다.
+        * - 포인트의 종류는 다양하다. 
+        * 
+        * 클래스 종류
+        * - Book 클래스
+        *   - 가격 
+        *   - 상품 코드
+        *   - 책 제목
+        * - OriginalBookPrice
+        *   
+        * - User 클래스, Book 클래스
+        *   - 테스트를 위해 익명 클래스를 선언한다.
+        * */
 
-            /// Basic Test
-            var book = new BookSelling.Book
-            {
-                Code = 12341123,
-                OriginPrice = 54000,
-                Title = "[YES24] 결제 로직을 잘 작성하는 방법"
-            };
+    /// Basic Test
+    var book = new BookSelling.Book
+    {
+        Code = 12341123,
+        OriginPrice = 54000,
+        Title = "[YES24] 결제 로직을 잘 작성하는 방법"
+    };
 
-            var obp = new BookSelling.OriginalBookPrice(book);
+    var obp = new BookSelling.OriginalBookPrice(book);
 
-            // 헌 책을 빌리는 경우
-            var bs = new BookSelling.BorrowingSale(new BookSelling.UsedBookSale(obp));
-            // 헌책 50 % 세일 후 렌탈 20 % 세일
-            var price = bs.GetPrice();
+    // 헌 책을 빌리는 경우
+    var bs = new BookSelling.BorrowingSale(new BookSelling.UsedBookSale(obp));
+    // 헌책 50 % 세일 후 렌탈 20 % 세일
+    var price = bs.GetPrice();
 
-            // 포인트 세일 구현되지 않음
-            var bps = new BookSelling.BookPointSale(bs);
-            // NotImplementedException 오류 발생
-            Assert.Throws<System.NotImplementedException>( () => bps.GetPrice());
+    // 포인트 세일 구현되지 않음
+    var bps = new BookSelling.BookPointSale(bs);
+    // NotImplementedException 오류 발생
+    Assert.Throws<System.NotImplementedException>( () => bps.GetPrice());
 
-            /* 예상 결과 치
-             * 54000 * 0.5 * 0.8
-             * */
-            WriteLine(price);
-            // 실제 계산 결과와 비교
-            Assert.AreEqual((uint)54000 * 0.5 * 0.8, price);
+    /* 예상 결과 치
+        * 54000 * 0.5 * 0.8
+        * */
+    WriteLine(price);
+    // 실제 계산 결과와 비교
+    Assert.AreEqual((uint)54000 * 0.5 * 0.8, price);
 
-            var UserInfo = new {
-                UserName = "김 건범",
-                Yes24Point = 5000,
-                Yes24Money = 1000,
-                UserID = "code-machina",
-                Email = "gbkim1988@gmail.com",
-                GitHub = "http://github.com/code-machina"
-            };
+    var UserInfo = new {
+        UserName = "김 건범",
+        Yes24Point = 5000,
+        Yes24Money = 1000,
+        UserID = "code-machina",
+        Email = "gbkim1988@gmail.com",
+        GitHub = "http://github.com/code-machina"
+    };
 
-            var Request = new
-            {
-                ToBeUsedYes24Point = 2000,
-            };
+    var Request = new
+    {
+        ToBeUsedYes24Point = 2000,
+    };
 
-            // Yes24 포인트 차감
-            if (!(UserInfo.Yes24Point >= Request.ToBeUsedYes24Point))
-                Assert.Throws<System.ArgumentOutOfRangeException>(
-                    // 익명 함수
-                    () => throw new System.ArgumentOutOfRangeException(
-                        message:$"{UserInfo.UserName} 님의 잔여 Yes24 포인트는 {UserInfo.Yes24Point} 입니다.", 
-                        paramName: nameof(Request.ToBeUsedYes24Point))
-                    );
+    // Yes24 포인트 차감
+    if (!(UserInfo.Yes24Point >= Request.ToBeUsedYes24Point))
+        Assert.Throws<System.ArgumentOutOfRangeException>(
+            // 익명 함수
+            () => throw new System.ArgumentOutOfRangeException(
+                message:$"{UserInfo.UserName} 님의 잔여 Yes24 포인트는 {UserInfo.Yes24Point} 입니다.", 
+                paramName: nameof(Request.ToBeUsedYes24Point))
+            );
 
-            var y24ps = new BookSelling.Yes24PointSale(bs, (uint)Request.ToBeUsedYes24Point);
+    var y24ps = new BookSelling.Yes24PointSale(bs, (uint)Request.ToBeUsedYes24Point);
 
-            var totalPrice = y24ps.GetPrice();
+    var totalPrice = y24ps.GetPrice();
 
-            // 산술 계산식과 비교
-            Assert.AreEqual(((uint)54000 * 0.5 * 0.8) - 2000, totalPrice);
-            WriteLine($"((uint)54000 * 0.5 * 0.8) - 2000 : {((uint)54000 * 0.5 * 0.8) - 2000}");
-            WriteLine($"totalPrice : {totalPrice}");
-        }
+    // 산술 계산식과 비교
+    Assert.AreEqual(((uint)54000 * 0.5 * 0.8) - 2000, totalPrice);
+    WriteLine($"((uint)54000 * 0.5 * 0.8) - 2000 : {((uint)54000 * 0.5 * 0.8) - 2000}");
+    WriteLine($"totalPrice : {totalPrice}");
+}
 
 
         [Test]
