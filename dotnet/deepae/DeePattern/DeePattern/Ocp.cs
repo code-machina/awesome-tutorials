960f7879ec89f4f80294adf5bfc2d6c5f61723f6
gbkim <gbkim1988@gmail.com>
2018-12-10 06:24:53 +0900

    Open/Closed Principle : Speicifcation Pattern ... Period !

--- dotnet/deepae/DeePattern/DeePattern/Ocp.cs
+++ dotnet/deepae/DeePattern/DeePattern/Ocp.cs
@@ -99,6 +99,7 @@ namespace DeePattern.SOLID.OCP.CORRECTNESS
         public string Name;
         public Color Color;
         public Size Size;
+        public int Price;
 
         /// <summary>
         /// 
@@ -106,11 +107,16 @@ namespace DeePattern.SOLID.OCP.CORRECTNESS
         /// <param name="name">유 </param>
         /// <param name="color"></param>
         /// <param name="size"></param>
-        public Product(string name, Color color, Size size)
+        public Product(string name, Color color, Size size, int price)
         {
             Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
             Color = color;
             Size = size;
+            Price = price;
+        }
+        public Product()
+        {
+
         }
     }
 
@@ -140,6 +146,16 @@ namespace DeePattern.SOLID.OCP.CORRECTNESS
         }
     }
 
+    public class GenericFilter<T>
+    {
+        public IEnumerable<T> Filter(IEnumerable<T> filtee, ISpecification<T> spec)
+        {
+            foreach (var item in filtee)
+                if (spec.IsSatisfiedBy(item))
+                    yield return item;
+        }
+    }
+
     /* Specification 패턴
      * Filter 를 수정 없이 사용할 수 있도록 정의해야한다.
      * 
@@ -160,11 +176,20 @@ namespace DeePattern.SOLID.OCP.CORRECTNESS
     }
     
 
+    /// <summary>
+    /// LINQ 를 활용할 수 있다.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class LinqSpecification<T>: CompositeSpecification<T>
     {
-
+        public abstract Expression<Func<T, bool>> AsExpression();
+        public override bool IsSatisfiedBy(T candidate) => AsExpression().Compile()(candidate);
     }
 
+    /// <summary>
+    /// Abstract Aggregate Composite Specification Class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class CompositeSpecification<T> : ISpecification<T>
     {
         public ISpecification<T> And(ISpecification<T> t1)
@@ -174,7 +199,7 @@ namespace DeePattern.SOLID.OCP.CORRECTNESS
 
         public ISpecification<T> AndNot(ISpecification<T> t1)
         {
-            throw new NotImplementedException();
+            return new AndNotSpecification<T>(this, t1);
         }
 
         /// <summary>
@@ -214,6 +239,87 @@ namespace DeePattern.SOLID.OCP.CORRECTNESS
         public override bool IsSatisfiedBy(T t1)
         {
             return left.IsSatisfiedBy(t1) && right.IsSatisfiedBy(t1);
+        }
+    }
+
+    public class AndNotSpecification<T> : CompositeSpecification<T>
+    {
+        ISpecification<T> left;
+        ISpecification<T> right;
+
+        public AndNotSpecification(ISpecification<T> left, ISpecification<T> right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override bool IsSatisfiedBy(T t1)
+        {
+            /* Truth Table
+             * !(A & B) 
+             * true true => false
+             * false true => true
+             * false false => true
+             */
+
+            return left.IsSatisfiedBy(t1) && right.IsSatisfiedBy(t1) != true;
+        }
+    }
+
+    public class OrSpecification<T> : CompositeSpecification<T>
+    {
+        ISpecification<T> left;
+        ISpecification<T> right;
+
+        public OrSpecification(ISpecification<T> left, ISpecification<T> right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override bool IsSatisfiedBy(T t1)
+        {
+            /* Truth Table
+             * !(A & B) 
+             * true true => false
+             * false true => true
+             * false false => true
+             */
+
+            return left.IsSatisfiedBy(t1) || right.IsSatisfiedBy(t1);
+        }
+    }
+    public class OrNotSpecification<T> : CompositeSpecification<T>
+    {
+        ISpecification<T> left;
+        ISpecification<T> right;
+
+        public OrNotSpecification(ISpecification<T> left, ISpecification<T> right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override bool IsSatisfiedBy(T t1)
+        {
+            /* Truth Table
+             * !(A & B) 
+             * true true => false
+             * false true => true
+             * false false => true
+             */
+
+            return left.IsSatisfiedBy(t1) || right.IsSatisfiedBy(t1) != true;
+        }
+    }
+
+    public class NotSpecification<T> : CompositeSpecification<T>
+    {
+        ISpecification<T> other;
+        public NotSpecification(ISpecification<T> other) => this.other = other;
+        public override bool IsSatisfiedBy(T t1)
+        {
+            return !other.IsSatisfiedBy(t1);
         }
     }
 
---
+++ dotnet/deepae/DeePattern/TestDeePa/TestSpecification.cs
@@ -0,0 +1,136 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using static System.Console;
+using DeePattern.SOLID.OCP.CORRECTNESS;
+
+namespace TestDeePa
+{
+    public class TestSpecification
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Filter_by_Specification()
+        {
+            /* 제품의 데이터를 조직하고 조건에 맞는 제품을 필터링하는 
+             * 상세 비즈니스 로직을 통해 다음의 시나리오를 만족한다.
+             * 
+             * */
+
+            /* 시나리오: 
+             * 개발자 A 는 사업부로 부터 다음의 개발 요청을 접수하였다. 
+             * 
+             * 상품에는 "가격", "출판사", "판매 권수", "수익률" 이 있습니다.
+             * 저희 사업부는 위의 네 가지 기준 이외에도 추가적인 조건을 걸 수 있는
+             * 검색 화면을 기획하여 이를 개발 요청을 드립니다.
+             * 
+             * 해석:
+             * Product 클래스는 위의 4 가지 외 1 개 필드를 더 가지고 있다. 
+             * 웹에서 개발하고 있으므로 추가 패턴은 작성하여 추가하면 그만이다.
+             * 
+             * 가격 정보는 수치(numeric)이므로 <, <= ,>=, == 등에 대한 연산이 필요하다.
+             * 문자열 정보는 단순 매칭이므로 문자열 비교 == 만 사용하면 된다. 
+             * 수익율은 double 혹은 float 이므로 별도의 로직이 필요할 것으로 생각된다. 
+             * 
+             * */
+
+            Product[] goods = new Product[]
+            {
+                new Product{Name="행복은 자아성취감",
+                    Color =Color.Blue,
+                    Size =Size.Large,
+                    Price =54000 },
+                new Product{Name="짧은 글의 행복",
+                    Color =Color.Red,
+                    Size =Size.Small,
+                    Price =12000 },
+                new Product{Name="변화하기, 변화관리",
+                    Color =Color.Green,
+                    Size =Size.Medium,
+                    Price =32000 },
+            };
+
+            // Product 의 가격이 20000 원 초과이고 
+            // 색깔이 Blue 인 책을 출력해주세요
+
+            var filter = new GenericFilter<Product>();
+            foreach (var item in filter.Filter(goods, new ColorSpec(Color.Blue).And(new GreaterThanSpec(20000))))
+            {
+                WriteLine(item.Name); // Expected -> output: "행복은 자아성취감"
+                Assert.AreSame(goods[0], item); // Expected -> True
+            }
+
+            foreach (var item in filter.Filter(goods, new PredicateSpec((t) => t.Color == Color.Green && t.Price > 30000 && t.Price < 40000)))
+            {
+                WriteLine(item.Name); // Expected -> output: "변화하기, 변화관리"
+                Assert.AreSame(goods[2], item); // Expected -> True
+            }
+        }
+    }
+
+    public class ColorSpec : CompositeSpecification<Product>
+    {
+
+        protected Color Color;
+        public ColorSpec(Color color)
+        {
+            this.Color = color;
+        }
+
+        public override bool IsSatisfiedBy(Product t1)
+        {
+            return t1.Color == Color;
+        }
+    }
+
+    public class PredicateSpec : CompositeSpecification<Product>
+    {
+        public Predicate<Product> predicate;
+
+        public PredicateSpec(Predicate<Product> predicate)
+        {
+            this.predicate = predicate;
+        }
+
+        public override bool IsSatisfiedBy(Product t1)
+        {
+            return predicate.Invoke(t1);
+        }
+    }
+
+
+    public class GreaterThanSpec : CompositeSpecification<Product>
+    {
+
+        protected int Price;
+        public GreaterThanSpec(int price)
+        {
+            this.Price = price;
+        }
+
+        public override bool IsSatisfiedBy(Product t1)
+        {
+            return t1.Price > Price;
+        }
+    }
+
+    public class EqualToSpec : CompositeSpecification<Product>
+    {
+
+        protected int Price;
+        public EqualToSpec(int price)
+        {
+            this.Price = price;
+        }
+
+        public override bool IsSatisfiedBy(Product t1)
+        {
+            return t1.Price == Price;
+        }
+    }
+}
