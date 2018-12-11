80f5eef31debf030cda48d2cd5b3c42d4a14306f
gbkim1988 <gbkim1988@gmail.com>
2018-12-11 18:00:45 +0900

    Hmm 좀무

---
+++ vuejs/my-first-nuxt-app/pages/user.vue
@@ -0,0 +1,25 @@
+<template>
+
+<v-app id="inspire">
+    <h1>This is the user.vue file </h1>
+    <v-toolbar app>
+      <v-toolbar-title>My Application</v-toolbar-title>
+    </v-toolbar>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-content>
+      <v-container fluid>
+        Hello World
+      </v-container>
+    </v-content>
+    <v-footer></v-footer>
+  </v-app>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+h1 {
+    background-color: black;
+}
+</style>
